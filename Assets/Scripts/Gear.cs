a4da8eebf33aa0cb506408d2af22f91d643b0fb7
shogonir <shogonir@gmail.com>
2018-01-15 00:35:36 +0900

    [add] Gear Scripts

---
+++ Assets/Scripts/Gear.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gear : MonoBehaviour
+{
+    [SerializeField]
+    private int numTooth = 12;
+
+    [SerializeField]
+    private List<GameObject> nextGearList = null;
+
+    [SerializeField]
+    private bool isConnectToNext = false;
+
+    public void Rotate(float angle, int prevNumTooth)
+    {
+        this.Rotate(angle * prevNumTooth / this.numTooth);
+    }
+
+    public void Rotate(float angle)
+    {
+        transform.Rotate(0, angle, 0);
+        this.RotateNextGear(angle);
+    }
+
+    private void RotateNextGear(float angle)
+    {
+        if (nextGearList == null)
+        {
+            return;
+        }
+
+        foreach (GameObject nextGear in this.nextGearList)
+        {
+            if (nextGear == null)
+            {
+                return;
+            }
+
+            Gear nextGearScript = nextGear.GetComponent<Gear>();
+            if (nextGearScript == null)
+            {
+                return;
+            }
+
+            if (this.isConnectToNext)
+            {
+                nextGearScript.Rotate(angle);
+            }
+            else
+            {
+                nextGearScript.Rotate(-angle, this.numTooth);
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/OriginGear.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OriginGear : MonoBehaviour
+{
+
+    [SerializeField]
+    private int numTooth = 15;
+
+    [SerializeField]
+    private GameObject nextGear = null;
+
+    [SerializeField]
+    private bool isConnectToNext = false;
+
+    [SerializeField]
+    private float timeTick = 0.4f;
+
+    private bool isActive = true;
+    private float timePrevious;
+
+    void Start()
+    {
+        this.timePrevious = Time.realtimeSinceStartup;
+    }
+
+    void Update()
+    {
+        float timeNow = Time.realtimeSinceStartup;
+        float timeSurplus = this.CalculateTimeSurplus(timeNow);
+        float timeSurplusPrevious = this.CalculateTimeSurplus(this.timePrevious);
+        float timeDelta = timeSurplus - timeSurplusPrevious + 0.000001f;
+        if (timeDelta >= this.timeTick)
+        {
+            int tooth = (int)(timeDelta / this.timeTick);
+            this.Rotate(-360f * tooth / this.numTooth);
+            this.timePrevious = timeNow;
+        }
+    }
+
+    private float CalculateTimeSurplus(float time)
+    {
+        return time - (time % this.timeTick);
+    }
+
+    public void Enable()
+    {
+        if (this.isActive)
+        {
+            return;
+        }
+        this.isActive = true;
+    }
+
+    public void Disable()
+    {
+        if (!this.isActive)
+        {
+            return;
+        }
+        this.isActive = false;
+    }
+
+    private void Rotate(float angle)
+    {
+        if (!this.isActive)
+        {
+            return;
+        }
+        transform.Rotate(0, angle, 0);
+        RotateNextGear(angle);
+    }
+
+    private void RotateNextGear(float angle)
+    {
+        if (this.nextGear == null)
+        {
+            return;
+        }
+
+        Gear nextGearScript = this.nextGear.GetComponent<Gear>();
+        if (nextGearScript == null)
+        {
+            return;
+        }
+
+        if (this.isConnectToNext)
+        {
+            nextGearScript.Rotate(angle);
+        }
+        else
+        {
+            nextGearScript.Rotate(-angle, this.numTooth);
+        }
+    }
+}
